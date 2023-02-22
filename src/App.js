bf8cd97c5f0bbfaf8a6920f412ab626f4d9c98b7
Sai Praveen Varma Mudunuri <saipraveenvarma.mudunuri@gmail.com>
2023-02-22 12:04:59 +0530

    step 3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
 import React from 'react'
+import  Banner  from "./components/Banner/Banner";
+import Navbar from "./components/Navbar/Navbar"
 
 function App() {
   return (
-    <div>App</div>
+  <div>
+    <Navbar/>
+  <Banner/>
+  </div>
+  
   )
 }
 
